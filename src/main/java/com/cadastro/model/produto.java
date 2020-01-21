0a555184d1bb32c7a5768cb393ab84d416d181a8
Flavio Luciano <fcastrofh@gmail.com>
2020-01-21 18:41:58 -0300

    Add Model Produto

---
+++ src/main/java/com/cadastro/model/produto.java
@@ -0,0 +1,32 @@
+package com.cadastro.model;
+
+public class produto {
+	private double preco;
+	private int desconto;
+	private String tipo;
+	
+	public double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(double p) {
+		preco = p;
+	}
+	
+	public int getDesconto() {
+		return desconto;
+	}
+	
+	public void setDesconto(int d) {
+		desconto = d;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	
+	public void setTipo(String t) {
+		tipo = t;
+	}
+
+}
