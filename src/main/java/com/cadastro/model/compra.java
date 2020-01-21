f8371af51f86309ec0a83ef0c9d36b33688847e3
Flavio Luciano <fcastrofh@gmail.com>
2020-01-21 18:44:51 -0300

    Add Model Compra

---
+++ src/main/java/com/cadastro/model/compra.java
@@ -0,0 +1,23 @@
+package com.cadastro.model;
+
+public class compra {
+	private carrinho carro;
+	private String nome;
+	
+	public carrinho getCarrinho() {
+		return carro;
+	}
+	
+	public void setCarrinho(carrinho c) {
+		carro = c;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String n) {
+		nome = n;
+	}
+
+}
